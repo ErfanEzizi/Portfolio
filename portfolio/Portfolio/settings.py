7bca618d07edc41eb144c0fa6d21e5621a2d03fc
Mozzie-atles <erfanezizi@protonmail.com>
2021-01-21 17:46:01 +0100

    0.5

--- portfolio/Portfolio/settings.py
+++ portfolio/Portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@11oepd7y2+s(w*$ew*sl%3lo74v5t#4b^op043qr$j+y)hhxq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['erfansportfolio.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
